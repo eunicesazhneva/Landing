9d1291c6ffe24b3a12ebfbaae2f99af8a16fa838
Eunice Sazhneva <eunicesazhneva@gmail.com>
2018-04-17 11:35:44 +0300

    flex header&footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-function myFunction() {
+function topNav() {
     var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function botNav() {
+    var x = document.getElementById("myBotnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
