b34ede42c3d9f52096d57761ec19d5b668820c36
Eunice Sazhneva <eunicesazhneva@gmail.com>
2018-04-19 13:20:52 +0300

    add slider & fix validation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,44 @@
 var invalid = document.getElementById('invalid-email');
+var email = document.getElementById('email');
 
-function validateEmail(mail) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
-        return (true)
+function validateEmail(mail) {    
+    var emailValidationRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return emailValidationRegex.test(email.value)
+}
+function isEmpty(val) {    
+    return (val.length === 0) 
+}
+
+
+function addClass() {
+    if (!isEmpty(email.value) || !validateEmail(email.value)) {
+        invalid.classList.toggle("invalid");
     } else {
         return (false)
     }
 }
-function addClass() {
-    if (!validateEmail()){
-        invalid.className += "invalid"
-    }    
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName('mySlides');
+    var dots = document.getElementsByClassName('dot');
+
+    if (n>slides.length){
+        slideIndex = 1
+    }   
+    for (i=0; i <slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for (i=0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].style.display = "active";
 }
